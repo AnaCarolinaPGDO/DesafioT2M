66bca39f3c00229c0c20da9c1bd01c8a7a326548
Ana Carolina <50124714+AnaCarolinaPGDO@users.noreply.github.com>
2025-01-11 14:01:41 -0300

    bugs2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import FormConsulta from './FormConsulta';
 import MarcarConsulta from './MarcarConsulta';
 
@@ -7,6 +7,7 @@ const App = () => {
     return (
         <Router>
             <Routes>
+                <Route path="/" element={<Navigate to="/form-consulta" />} />
                 <Route path="/form-consulta" element={<FormConsulta />} />
                 <Route path="/marcar-consulta" element={<MarcarConsulta />} />
             </Routes>
